c1461b3db5663d3279a88c524573fdc2858ae389
Adam Fontenot <adam.m.fontenot@gmail.com>
2018-07-12 23:20:48 -0700

    fix typo

--- tcobot.user.js
+++ tcobot.user.js
@@ -10,10 +10,14 @@
 // Thanks to https://github.com/tony-o/t.co-bypass
 // for the basic idea behind this script.
 
+// If the script isn't replacing URLs for you,
+// it's probably because of a Firefox bug.
+// Discussion / workaround: https://github.com/violentmonkey/violentmonkey/issues/408
+
 var tcobot = "https://tco.bot.nu"
 
 var mut = new MutationObserver(function(mutations) {
-  var links = document.body.getElementsBylinkName("a");
+  var links = document.body.getElementsByTagName("a");
   for (var i = 0; i < links.length; i++) {
     var link = links[i];
     if (link.getAttribute("class") !== null && link.getAttribute("class").indexOf("twitter-atreply") > -1) {
