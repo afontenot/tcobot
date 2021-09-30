6e54d468d7df884482134361c71eb5434834f167
Adam Fontenot <adam.m.fontenot@gmail.com>
2021-09-30 15:37:44 -0700

    fix chained redirections and strip tracking parameters from URLs

--- tcobot.py
+++ tcobot.py
@@ -3,11 +3,45 @@ import requests
 
 from flask import Flask, abort, redirect
 from sqlitedict import SqliteDict
+from urllib.parse import urlencode, urlunparse, urlparse, parse_qs
 
 app = Flask(__name__)
 urls = SqliteDict('./urls.sqlite', autocommit=True)
 
 ua = {'User-Agent': 'HTTPie/0.9.9'}
+
+strip_params = ["utm_source", "utm_medium", "utm_term", "utm_content", "utm_campaign", 
+                "utm_reader", "utm_place", "utm_userid", "utm_cid", "utm_name", 
+                "utm_pubreferrer", "utm_swu", "utm_viz_id", "ga_source", "ga_medium", 
+                "ga_term", "ga_content", "ga_campaign", "ga_place", "yclid", 
+                "_openstat", "fb_action_ids", "fb_action_types", "fb_ref", "fb_source", 
+                "action_object_map", "action_type_map", "action_ref_map", "gs_l", 
+                "pd_rd_r@amazon.*", "pd_rd_w@amazon.*", "pd_rd_wg@amazon.*", 
+                "_encoding@amazon.*", "psc@amazon.*", "ved@google.*", "ei@google.*", 
+                "sei@google.*", "gws_rd@google.*", "cvid@bing.com", "form@bing.com", 
+                "sk@bing.com", "sp@bing.com", "sc@bing.com", "qs@bing.com", 
+                "pq@bing.com", "feature@youtube.com", "gclid@youtube.com", 
+                "kw@youtube.com", "ref@amazon.*", "_hsenc", "mkt_tok", "hmb_campaign", 
+                "hmb_source", "hmb_medium", "fbclid", "spReportId", "spJobID", 
+                "spUserID", "spMailingID", "utm_mailing", "utm_brand", "CNDID", "mbid", 
+                "trk", "trkCampaign", "sc_campaign", "sc_channel", "sc_content", 
+                "sc_medium", "sc_outcome", "sc_geo", "sc_country", "wt_zmc"]
+
+def unredirect(url):
+    r = requests.get(url, headers=ua)
+    return r.url
+
+def clean(url):
+    url = urlparse(url)
+    query = parse_qs(url.query)
+    newquery = query.copy()
+    for key in query.keys(): # we're modifying dictionary, so copy keys
+        for param in strip_params:
+            if re.search(param, key):
+                del newquery[key]
+    return urlunparse((url.scheme, url.netloc, url.path, url.params,
+                       urlencode(newquery, doseq=True), url.fragment))
+
 
 @app.route("/<tcourl>")
 def get(tcourl):
@@ -21,7 +55,7 @@ def get(tcourl):
         else:
             r = requests.get("https://t.co/" + tcourl, headers=ua, allow_redirects=False)
             if r.status_code == 301:
-                url = r.headers['location']
+                url = clean(unredirect(r.headers['location']))
                 urls[tcourl] = url
                 return redirect(url, 301)
             elif r.status_code == 200:
